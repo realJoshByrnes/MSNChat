33504d62c81681e67e7461ea08267cbaabc822e3
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-10-30 11:09:31 +1100

    Start of WhisperWindow tracking

--- MSNChat/ChatRoom.cs
+++ MSNChat/ChatRoom.cs
@@ -1,4 +1,11 @@
+using Microsoft.Win32.SafeHandles;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.Accessibility;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace MSNChat
 {
@@ -12,7 +19,39 @@ namespace MSNChat
 
     private void chatFrame1_OcxHandleCreated(object? sender, EventArgs e)
     {
-      Debug.WriteLine("chatFrame1_OcxHandleCreated", chatFrame1.OcxHandle.ToString("X8"));
+      HWND hwndCWM = PInvoke.FindWindowEx((HWND)chatFrame1.OcxHandle, (HWND)IntPtr.Zero, "CWhisperManager", null);
+
+      uint threadCWM;
+      uint processCWM; // We don't actually need this, but it's worth keeping to show why it needs to be unsafe.
+      unsafe {
+        threadCWM = PInvoke.GetWindowThreadProcessId(hwndCWM, &processCWM);
+      }
+
+      PInvoke.SetWindowsHookEx(WINDOWS_HOOK_ID.WH_CALLWNDPROCRET, new HOOKPROC(HookProc), (HINSTANCE)0, threadCWM);
+    }
+
+    LRESULT HookProc(int code, WPARAM wParam, LPARAM lParam)
+    {
+      if (code < 0)
+        return PInvoke.CallNextHookEx(null, code, wParam, lParam);
+
+      CWPRETSTRUCT cwp = Marshal.PtrToStructure<CWPRETSTRUCT>(lParam);
+      if (cwp.message == (uint) /* WM_CREATE */ 0x1)
+      {
+        if (WinUtils.isDialogWindow(cwp.hwnd))
+          Debug.WriteLine($"CREATE {cwp.hwnd} - {WinUtils.GetWindowText(cwp.hwnd)}");
+      }
+      else if (cwp.message == (uint) /* WM_DESTROY */ 0x2)
+      {
+        if (WinUtils.isDialogWindow(cwp.hwnd))
+          Debug.WriteLine($"DESTROY {cwp.hwnd} - {WinUtils.GetWindowText(cwp.hwnd)}");
+      }
+      else if (cwp.message == (uint) /* WM_SETTEXT */ 0xc)
+      {
+        if (WinUtils.isDialogWindow(cwp.hwnd))
+          Debug.WriteLine($"SETTEXT {cwp.hwnd} - {WinUtils.GetWindowText(cwp.hwnd)}");
+      }
+      return PInvoke.CallNextHookEx(null, code, wParam, lParam);
     }
   }
 }
---
+++ MSNChat/WinUtils.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace MSNChat
+{
+  internal static class WinUtils
+  {
+    public static String getClassName(HWND hWnd)
+    {
+      string className = "";
+      unsafe
+      {
+        // The maximum length for lpszClassName is 256.
+        var classNameLen = 256;
+        IntPtr strMem = Marshal.AllocHGlobal((classNameLen + 1) * sizeof(char));
+        PWSTR lpClassName = new PWSTR((char*)strMem);
+
+        if (PInvoke.GetClassName(hWnd, lpClassName, classNameLen + 1) != 0)
+        {
+          className = lpClassName.ToString();
+        }
+        Marshal.FreeHGlobal(strMem);
+      }
+      return className;
+    }
+
+    public static string GetWindowText(HWND hWnd)
+    {
+      string windowText = "";
+      int stringLen = PInvoke.GetWindowTextLength(hWnd);
+      if (stringLen != 0)
+      unsafe {
+        IntPtr stringMem = Marshal.AllocHGlobal((stringLen + 1) * sizeof(char));
+        PWSTR lpString = new PWSTR((char*)stringMem);
+        if (PInvoke.GetWindowText(hWnd, lpString, stringLen + 1) != 0)
+        {
+          windowText = lpString.ToString();
+        }
+        Marshal.FreeHGlobal(stringMem);
+      }
+      return windowText;
+    }
+
+    public static bool isDialogWindow(HWND hWnd)
+    {
+      return getClassName(hWnd) == "#32770";
+    }
+  }
+}
