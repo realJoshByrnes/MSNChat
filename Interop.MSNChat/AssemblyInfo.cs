a3f35daa978cbbc711f163b39b7512e3f2ef9292
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-11-03 22:13:11 +1100

    Upgrade Interop.MSNChat to .net 8.0
    - Upgrade Interop.MSNChat lib to .net8

--- Interop.MSNChat/AssemblyInfo.cs
+++ Interop.MSNChat/AssemblyInfo.cs
@@ -6,4 +6,3 @@ using System.Security;
 [assembly: TypeLibVersion(1, 0)]
 [assembly: SecurityRules(SecurityRuleSet.Level2)]
 [assembly: Guid("0f0a655c-6c6d-4e0b-8038-f980b36f9c78")]
-[assembly: AssemblyVersion("1.0.0.0")]
