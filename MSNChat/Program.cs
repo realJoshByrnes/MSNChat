86ece6abefbdca99c911b2c82442d845823a2eb5
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-11-03 23:21:32 +1100

    - Enable DEP
    - Enable DEP
    - Enable ATL thunking explicitly.

--- MSNChat/Program.cs
+++ MSNChat/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.Win32.SafeHandles;
+using System.Diagnostics;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.System.Threading;
+
 namespace MSNChat
 {
   internal static class Program
@@ -10,6 +16,13 @@ namespace MSNChat
     {
       // To customize application configuration such as set high DPI settings or default font,
       // see https://aka.ms/applicationconfiguration.
+
+      if (PInvoke.SetProcessDEPPolicy(PROCESS_DEP_FLAGS.PROCESS_DEP_ENABLE) != 1)
+      {
+        MessageBox.Show("Failed to modify DEP policy for this process.", "MSN Chat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      };
+      // TODO: GetProcessDEPPolicy. We can display more information if PROCESS_DEP_FLAGS.PROCESS_DEP_DISABLE_ATL_THUNK_EMULATION is set (as this will cause crashes).
+
       ApplicationConfiguration.Initialize();
       Application.Run(new MDIChatClient());
     }
