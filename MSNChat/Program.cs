cb06adc0ee30e53494c941f29c4a303b8b3d050d
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2024-11-04 04:57:53 +1100

    - Check DEP flags
    - Ensure DEP is enabled for this process with ATL thunk emulation.
    - Display error and exit if unable to set DEP and ATL thunk emulation is disabled.

--- MSNChat/Program.cs
+++ MSNChat/Program.cs
@@ -17,11 +17,23 @@ namespace MSNChat
       // To customize application configuration such as set high DPI settings or default font,
       // see https://aka.ms/applicationconfiguration.
 
-      if (PInvoke.SetProcessDEPPolicy(PROCESS_DEP_FLAGS.PROCESS_DEP_ENABLE) != 1)
+      if (!PInvoke.SetProcessDEPPolicy(PROCESS_DEP_FLAGS.PROCESS_DEP_ENABLE))
       {
-        MessageBox.Show("Failed to modify DEP policy for this process.", "MSN Chat", MessageBoxButtons.OK, MessageBoxIcon.Error);
-      };
-      // TODO: GetProcessDEPPolicy. We can display more information if PROCESS_DEP_FLAGS.PROCESS_DEP_DISABLE_ATL_THUNK_EMULATION is set (as this will cause crashes).
+        // Get the current process handle
+        using (SafeProcessHandle hProcess = new SafeProcessHandle(Process.GetCurrentProcess().Handle, false))
+        {
+          // Call GetProcessDEPPolicy
+          if (PInvoke.GetProcessDEPPolicy(hProcess, out uint flags, out BOOL permanent))
+          {
+            PROCESS_DEP_FLAGS depFlags = (PROCESS_DEP_FLAGS)flags;
+            if ((depFlags & PROCESS_DEP_FLAGS.PROCESS_DEP_DISABLE_ATL_THUNK_EMULATION) != 0)
+            {
+              MessageBox.Show("This computer's Data Execution Policy will not allow the MSN Chat Control to load.\nPlease disable DEP for this process and try again.", "MSN Chat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+              return;
+            }
+          }
+        }
+      }
 
       ApplicationConfiguration.Initialize();
       Application.Run(new MDIChatClient());
